1481a6856762f0ae99297e5048c516064b419276
Daniil Vinogradov <xitrix@bk.ru>
2022-06-20 10:18:58 +0300

    Injected fix

--- MVVMFoundation/Classes/MVVM/Container/Injected.swift
+++ MVVMFoundation/Classes/MVVM/Container/Injected.swift
@@ -5,13 +5,13 @@
 //  Created by Даниил Виноградов on 18.06.2022.
 //
 
-import MVVMFoundation
-
 @propertyWrapper
-struct Injected<T: Any> {
-    var wrappedValue: T {
+public struct Injected<T: Any> {
+    public var wrappedValue: T {
         get {
             MVVM.shared.container.resolve() as T
         }
     }
+
+    public init() {}
 }

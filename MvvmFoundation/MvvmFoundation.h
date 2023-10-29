142665525faf818a2f928e5368c9329442f86079
Daniil Vinogradov <daniilvinogradov@cc.com.mt>
2023-10-29 21:17:13 +0100

    Initial Commit

---
+++ MvvmFoundation/MvvmFoundation.h
@@ -0,0 +1,18 @@
+//
+//  MvvmFoundation.h
+//  MvvmFoundation
+//
+//  Created by Daniil Vinogradov on 29/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MvvmFoundation.
+FOUNDATION_EXPORT double MvvmFoundationVersionNumber;
+
+//! Project version string for MvvmFoundation.
+FOUNDATION_EXPORT const unsigned char MvvmFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MvvmFoundation/PublicHeader.h>
+
+

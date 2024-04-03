dbcda43f41ae2379dab964fbab2ed06543ac7989
Daniil Vinogradov <xitrix@bk.ru>
2024-04-04 00:19:56 +0200

    Add tvOS support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
  
 let package = Package(
     name: "MvvmFoundation",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .tvOS(.v14)],
     products: [
         .library(
             name: "MvvmFoundation",

e693104ca459f8e94cbc3067a183282a5a039459
Daniil Vinogradov <xitrix@bk.ru>
2024-04-09 20:23:54 +0200

    Mark StrictConcurrency feature as completed

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "MvvmFoundation"
+            name: "MvvmFoundation",
+            swiftSettings: [
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
         )
     ]
 )
